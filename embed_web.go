9981eed918157766eab10cf6dafde89e924e0d7a
52op <letvar@qq.com>
2026-05-05 19:35:35 +0800

    fix: go:embed all:web/static 改为 go:embed all:web

--- embed_web.go
+++ embed_web.go
@@ -7,7 +7,7 @@ import (
 	"io/fs"
 )
 
-//go:embed all:web/static
+//go:embed all:web
 var embeddedStatic embed.FS
 
 // WebFS provides access to embedded web static files. Nil when not built with embed tag.
